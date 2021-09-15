8442f62d2bd8dbca696111ca47bebb4d10e61b63
Kris Renaldi <krenaldi@comcast.net>
2021-09-15 08:15:43 -0400

    Setup request authorization from Spotify on login route

--- server.js
+++ server.js
@@ -1,20 +1,43 @@
 const express = require('express');
+const querystring = require('querystring');
+require('dotenv').config();
+const axios = require('axios');
+
+console.log(process.env.CLIENT_ID);
+
 const app = express();
 
 const PORT = 8888;
 
-app.get('/', (req, res) => {
-  const data = {
-    name: 'Kris',
-    isAwesome: true
-  }
-  res.json(data);
-});
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
-app.get('/awesome-generator', (req, res) => {
-  console.log(req.query);
-  const { name, isAwesome } = req.query;
-  res.send(`${name} is ${JSON.parse(isAwesome) ? 'really' : 'not'} awesome`);
+const generateRandomString = length => {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+const stateKey = 'spotify_auth_state';
+
+app.get('/login', (req, res) => {
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  const scope = 'user-read-private user-read-email';
+
+  const queryParams = querystring.stringify({
+    client_id: CLIENT_ID,
+    response_type: 'code',
+    redirect_uri: REDIRECT_URI,
+    state: state,
+    scope: scope
+  })
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 })
 
 app.listen(PORT, () => {
