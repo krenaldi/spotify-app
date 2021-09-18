76b09e28c5589ac792a54f7d300a8c270121f8bd
Kris Renaldi <krenaldi@comcast.net>
2021-09-18 15:17:57 -0400

    Able to fetch data from Spotify and display name, # of followers, and profile pic from Spotify

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,54 @@
-import { useEffect } from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { accessToken, logout, getCurrentUserProfile } from './spotify';
 import './App.css';
 
 function App() {
+  const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
 
   useEffect(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const accessToken =urlParams.get('access_token');
-    const refreshToken = urlParams.get('refresh_token');
+    setToken(accessToken);
 
-    console.log(accessToken);
-    console.log(refreshToken);
+    const fetchData = async () => {
+      try {
+        const { data } = await getCurrentUserProfile();
+        setProfile(data);
 
-    if (refreshToken) {
-      fetch(`/refresh_token?refresh_token=${refreshToken}`)
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.log(err))
+        console.log(data)
+
+      } catch (error) {
+        console.error(error);
+      }
     }
+
+    fetchData();
   }, [])
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="http://localhost:8888/login"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Login to Spotify
-        </a>
+        {!token ? (
+          <a
+            className="App-link"
+            href="http://localhost:8888/login"
+          >
+            Login to Spotify
+          </a>
+        ) : (
+          <>
+            <h1>Logged in!</h1>
+            <button onClick={logout}>Log Out</button>
+            {profile && (
+              <div>
+                <h1>{profile.display_name}</h1>
+                <p>{profile.followers.total} Followers</p>
+                {profile.images.length && profile.images[0].url && (
+                  <img src={profile.images[0].url} alt="Avatar" />
+                )}
+              </div>
+            )}
+          </>
+        )}
       </header>
     </div>
   );
--- client/src/spotify.js
+++ client/src/spotify.js
@@ -107,3 +107,10 @@ const getAccessToken =() => {
 };
 
 export const accessToken = getAccessToken();
+
+/* Axios global request headers */
+axios.defaults.baseURL = 'https://api.spotify.com/v1';
+axios.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
+axios.defaults.headers['Content-Type'] = 'application/json';
+
+export const getCurrentUserProfile = () => axios.get('/me');
