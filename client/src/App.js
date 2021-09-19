3aeeab668a79a193c7d098e4008cc3c3fc62de69
Kris Renaldi <krenaldi@comcast.net>
2021-09-19 01:39:18 -0400

    Created Profile page and added Header to top

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,55 @@
 import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch, useLocation } from 'react-router-dom';
-import { accessToken, logout, getCurrentUserProfile } from './spotify';
-import { catchErrors } from './utils';
+import { accessToken, logout } from './spotify';
 import { GlobalStyle } from './styles';
-import { Login } from './pages';
+import { Login, Profile } from './pages';
+import styled from 'styled-components/macro';
+
+const LogoutButton = styled.button`
+  position: absolute;
+  top: var(--spacing-sm);
+  right: var(--spacing-md);
+  padding: var(--spacing-xs) var(--spacing-sm);
+  background-color: rgba(0,0,0,.7);
+  color: var(--white);
+  font-size: var(--fz-sm);
+  font-weight: 700;
+  border-radius: var(--border-radius-pill);
+  z-index: 10;
+  @media (min-width: 768px) {
+    right: var(--spacing-lg);
+  }
+`
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname])
+
+  return null;
+};
 
 function App() {
   const [token, setToken] = useState(null);
-  const [profile, setProfile] = useState(null);
 
   // Scroll to the top when changing routes
-  function ScrollToTop() {
-    const { pathname } = useLocation();
-
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, [pathname])
-
-    return null;
-  }
 
   useEffect(() => {
     setToken(accessToken);
-
-    const fetchData = async () => {
-      const { data } = await getCurrentUserProfile();
-      // console.log(data);
-      setProfile(data);
-    };
-    catchErrors(fetchData());
   }, [])
 
   return (
     <div className="App">
       <GlobalStyle />
-      <header className="App-header">
-        {!token ? (
-          <Login />
-        ) : (
+      {!token ? (
+        <Login />
+      ) : (
+        <>
+          <LogoutButton onClick={logout}>
+            Log Out
+          </LogoutButton>
           <Router>
             <ScrollToTop />
             <Switch>
@@ -53,25 +65,13 @@ function App() {
               <Route path="/playlists">
                 <h1>Playlists</h1>
               </Route>
-
               <Route path="/">
-                <>
-                  <button onClick={logout}>Log Out</button>
-                  {profile && (
-                    <div>
-                      <h1>{profile.display_name}</h1>
-                      <p>{profile.followers.total} Followers</p>
-                      {profile.images.length && profile.images[0].url && (
-                        <img src={profile.images[0].url} alt="Avatar" />
-                      )}
-                    </div>
-                  )}
-                </>
+                <Profile />
               </Route>
             </Switch>
           </Router>
-        )}
-      </header>
+        </>
+      )}
     </div>
   );
 }
