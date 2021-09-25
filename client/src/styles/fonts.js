1ac195ee55dded9b0f98584261c584dcc40c2bc8
Kris Renaldi <krenaldi@comcast.net>
2021-09-25 11:51:27 -0400

    Added Montserrat font as global font

--- client/src/styles/fonts.js
+++ client/src/styles/fonts.js
@@ -2,25 +2,22 @@ import { css } from 'styled-components/macro';
 
 const fonts = css`
   @font-face {
-    font-family: 'Circular Std';
-    src: url('../fonts/CircularStd-Book.woff2') format('woff2'),
-    url('../fonts/CircularStd-Book.woff') format('woff');
+    font-family: 'Montserrat';
+    src: url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400&display=swap');
     font-weight: 400;
     font-style: normal;
   }
 
   @font-face {
-    font-family: 'Circular Std';
-    src: url('../fonts/CircularStd-Bold.woff2') format('woff2'),
-    url('../fonts/CircularStd-Bold.woff') format('woff');
+    font-family: 'Montserrat';
+    src: url('https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap');
     font-weight: 700;
     font-style: normal;
   }
 
   @font-face {
-    font-family: 'Circular Std';
-    src: url('../fonts/CircularStd-Black.woff2') format('woff2'),
-    url('../fonts/CircularStd-Black.woff') format('woff');
+    font-family: 'Montserrat';
+    src: url('https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap');
     font-weight: 900;
     font-style: normal;
   }
